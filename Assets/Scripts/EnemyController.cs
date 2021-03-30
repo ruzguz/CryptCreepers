60c3dcaad9e5fb5e54289a8755f06b8b3bb35e41
Rusben Guzman <rusbenguzman@gmail.com>
2021-03-29 21:00:33 -0400

    Make enemy follow player

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -7,17 +7,22 @@ public class EnemyController : MonoBehaviour
 
     // General vars
     [SerializeField] private int health = 1;
+    [SerializeField] private float _speed = 1;
+    private Transform _player;
     
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _player = FindObjectOfType<PlayerController>().transform;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Make enemy follow the player
+        Vector2 direction = _player.position - transform.position;
+        transform.position += (Vector3)direction.normalized * Time.deltaTime * _speed;
         
     }
 
