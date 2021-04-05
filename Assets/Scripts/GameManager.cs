3549e7854ed6e42aacaf0574d8f5057e225bcb14
Rusben Guzman <rusbenguzman@gmail.com>
2021-04-05 19:25:13 -0400

    Create game manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Singleton
+    public static GameManager instance;
+
+    // General vars
+    [SerializeField] private int _time = 30;
+    
+    private void Awake() {
+        if (instance == null) 
+        {
+            instance = this;
+        } else 
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start() {
+        StartCoroutine(TimeCountdown());
+    }
+
+    IEnumerator TimeCountdown() 
+    {
+        while (_time > 0) 
+        {
+            yield return new WaitForSeconds(1);
+            _time--;
+        }
+    }
+}
