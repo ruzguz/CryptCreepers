cdfde3346e9425f07c91700bdc81ddc0adec4c0c
Rusben Guzman <rusbenguzman@gmail.com>
2021-04-08 22:14:34 -0400

    Create spawn powerup function

--- Assets/Scripts/ItemSpawnerController.cs
+++ Assets/Scripts/ItemSpawnerController.cs
@@ -5,23 +5,38 @@ using UnityEngine;
 public class ItemSpawnerController : MonoBehaviour
 {
     // General vars
-    [SerializeField] private int _spawnTime = 5;
+    [SerializeField] private int _spawnItemTime = 5;
     [SerializeField] private int _spawnRadius = 5;
+    [SerializeField] private int _spawnPowerUpTime = 5;
     [SerializeField] private GameObject _checkpointPrefab;
+    [SerializeField] private GameObject[] _powerUpPrefabs;
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(SpawnItem());
+        StartCoroutine(SpawnItemRoutine());
+        StartCoroutine(SpawnPowerUpRoutine());
     }
 
-    IEnumerator SpawnItem()
+    IEnumerator SpawnItemRoutine()
     {
         while (true) 
         {
-            yield return new WaitForSeconds(_spawnTime);
+            yield return new WaitForSeconds(_spawnItemTime);
             Vector2 randomPosition = Random.insideUnitCircle * _spawnRadius;
             Instantiate(_checkpointPrefab, randomPosition, Quaternion.identity);
         }
 
+    }
+
+    IEnumerator SpawnPowerUpRoutine() 
+    {
+        while (true) 
+        {
+            yield return new WaitForSeconds(_spawnPowerUpTime);
+            Vector2 randomPosition = Random.insideUnitCircle * _spawnRadius;
+            int randomPowerUp = Random.Range(0, _powerUpPrefabs.Length);
+            Instantiate(_powerUpPrefabs[randomPowerUp], randomPosition, Quaternion.identity);
+
+        }
     }
 }
